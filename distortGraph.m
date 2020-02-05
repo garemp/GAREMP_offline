c480035d2d58c8b50de811f21f4c4e913cbd35b7
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-05 12:10:11 +0800

    Update distortGraph.m

--- distortGraph.m
+++ distortGraph.m
@@ -30,4 +30,58 @@ end
 savejson([], djson, 'maps_carto.json');
 add_brackets('maps_carto.json');
 
+xt = (xx - center_nocarto(1)) / radium_nocarto * 100;
+yt = (yy - center_nocarto(2)) / radium_nocarto * 100;
+[idx, d] = knnsearch([xt; yt]', [xt; yt]', 'K', 2);
+mindforall = min(d(:, 2));
+
+disp('calclating metrics for nodes in graph ...')
+tic
+for i = 1:size(xx, 2)
+    xt = xx(i);
+    yt = yy(i);
+    xt = (xt - center_nocarto(1)) / radium_nocarto * 100;
+    yt = (yt - center_nocarto(2)) / radium_nocarto * 100;
+    mind = 1000;
+    for j = 1:size(edges, 2)
+        e = edges{j};
+        [ s, t ] = fun_edge_curve( nodes, e );
+        [x, y] = fun_js_curve(s, t);
+        x = (x - center_nocarto(1)) / radium_nocarto * 100;
+        y = (y - center_nocarto(2)) / radium_nocarto * 100;
+        [idx, d] = knnsearch([x; y]', [xt, yt]);
+        if d < 1e-6 && (idx == 1 || idx == size(x, 2))
+            dtvert_count(i) = dtvert_count(i) + 0.1;
+            dist = sqrt((x(1) - x(end)) ^ 2 + (y(1) - y(end)) ^ 2);
+            if dist < mind
+                mind = dist;
+            end
+            continue;
+        end
+        dtvert_count(i) = dtvert_count(i) + 1 / (d + 1);            
+    end
+    dtvert_count(i) = dtvert_count(i) + 1 / (mind / mindforall + 1);
+end
+toc
+
+dt_count = zeros(size(DT, 1), 1);
+for i = 1:size(DT, 1)
+    idx = DT(i, :);
+    dt_count(i) = dt_count(i) + dtvert_count(idx(1)); 
+    dt_count(i) = dt_count(i) + dtvert_count(idx(2)); 
+    dt_count(i) = dt_count(i) + dtvert_count(idx(3)); 
+end
+
+comma = repmat(',', size(dt_count, 1), 1);
+ret = repmat(sprintf('\n'), size(dt_count, 1), 1);
+state = repmat('state ', size(dt_count, 1), 1);
+str = [num2str([1:size(DT, 1)]'), comma, num2str(round(dt_count*10)), ret];
+
+fp = fopen(['dt_count.csv'], 'w');
+fwrite(fp, ['Region.Id,Region.Data', sprintf('\n')], 'char');
+fwrite(fp, str', 'char');
+fclose(fp);
+
+system(['./cartogram -eig maps_carto.json -a dt_count.csv']);
+
 end
