ff44f4d138d7989a446a2e7bd19458f66b35930a
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-05 21:58:38 +0800

    Update garemp_offline.m

--- garemp_offline.m
+++ garemp_offline.m
@@ -1,9 +1,10 @@
 function [  ] = garemp_offline(  )
 
-addArrows('maps.json', 'maps+arrows.json');
+addArrows('maps+sample.json', 'maps+arrows.json');
 flipEdges('maps+arrows.json', 'maps+flipped.json');
 distortGraph('maps+flipped.json', 'maps+distort.json');
 flipEdges('maps+distort.json', 'maps+mono.json');
 addColors('maps+mono.json', 'maps+color.json');
+addInfo('maps+color.json', 'maps+results.json');
 
 end
