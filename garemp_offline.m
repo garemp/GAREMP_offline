209f76d91c4a4646ff51ee3c0a4c3d8a3c9e04da
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-14 23:01:49 +0800

    Update garemp_offline.m

--- garemp_offline.m
+++ garemp_offline.m
@@ -1,4 +1,13 @@
 function [  ] = garemp_offline(  )
+% Apache-2.0 © 2019-2020 Jin Yan
+% School of Management and Engineering, 
+% Capital University of Economics and Business, Beijing, China
+% Copyright @ 2019-2020
+% All Right Reserved
+% GAREMP: Graphical Attribute and Relationship Explorer for Map Projections
+% http://garemp.github.io/
+%
+% Run GAREMP offline in one command
 
 addArrows('maps+sample.json', 'maps+arrows.json');
 flipEdges('maps+arrows.json', 'maps+flipped.json');
