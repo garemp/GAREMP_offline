8134f7994156f04c812d9b8fc34b61546d977a39
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-14 22:59:15 +0800

    Update fun_edge_curve.m

--- fun_edge_curve.m
+++ fun_edge_curve.m
@@ -1,4 +1,12 @@
 function [ s, t ] = fun_edge_curve( nodes, e )
+% Apache-2.0 © 2019-2020 Jin Yan
+% School of Management and Engineering, 
+% Capital University of Economics and Business, Beijing, China
+% Copyright @ 2019-2020
+% All Right Reserved
+% GAREMP: Graphical Attribute and Relationship Explorer for Map Projections
+% http://garemp.github.io/
+%
 % find the source node 's' and target node 't' for given edge 'e'
 
 s = [];
