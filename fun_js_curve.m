cbe0879a9098f8e53da17de862a5ffa84671b6a5
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-14 23:00:49 +0800

    Update fun_js_curve.m

--- fun_js_curve.m
+++ fun_js_curve.m
@@ -1,4 +1,14 @@
 function [ x, y ] = fun_js_curve( s, t )
+% Apache-2.0 © 2019-2020 Jin Yan
+% School of Management and Engineering, 
+% Capital University of Economics and Business, Beijing, China
+% Copyright @ 2019-2020
+% All Right Reserved
+% GAREMP: Graphical Attribute and Relationship Explorer for Map Projections
+% http://garemp.github.io/
+%
+% Formula for generating Bezier curve
+% cp is a control point
 
 cp = [(s.x + t.x) / 2 + (t.y - s.y) / 4, (s.y + t.y) / 2 + (s.x - t.x) / 4];
 tp = [0:0.01:1];
