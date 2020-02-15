1e68bd4b051caad518eee3e57f9f77556578e308
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-15 16:44:31 +0800

    Update calc_edge_matrix.m

--- calc_edge_matrix.m
+++ calc_edge_matrix.m
@@ -19,7 +19,7 @@ function [ edges_matrix ] = calc_edge_matrix( xx, yy, DT )
 % Six edges connect N and its neighbor vertice
 % calc_edge_matrix return a matrix, the Nth row of the matrix is like this 
 %     9    23    36    47    68    80     0     0     0     0
-% The neighbor vertice of N is obtained in ascending order
+% From that row, the neighbor vertice of N is obtained in ascending order with zero filling at tail
 
 edges_matrix = [];
 DT_edges = compute_edges(DT);
@@ -31,7 +31,6 @@ for i = 1:size(xx, 2)
     ff = sort([op1, op2]);
     edges_matrix(end + 1, :) = [ff, zeros(1, 10 - size(ff, 2))];
 end
-
 
 end
 
