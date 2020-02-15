9bce932549f35bfc634ba971d364953738385e9e
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-15 16:42:06 +0800

    Update calc_edge_matrix.m

--- calc_edge_matrix.m
+++ calc_edge_matrix.m
@@ -9,6 +9,17 @@ function [ edges_matrix ] = calc_edge_matrix( xx, yy, DT )
 %
 % Calculate adjacent edges for given node in a triangulated network, save the result in a matrix
 % Each row represents all adjacent edges for a node
+%
+% For a given node N, it is the common vertex for six faces, just like this
+%         23  --  9
+%        /  \   /  \
+%       68 -- N -- 36
+%        \  /   \  /
+%         80  -- 47
+% Six edges connect N and its neighbor vertice
+% calc_edge_matrix return a matrix, the Nth row of the matrix is like this 
+%     9    23    36    47    68    80     0     0     0     0
+% The neighbor vertice of N is obtained in ascending order
 
 edges_matrix = [];
 DT_edges = compute_edges(DT);
